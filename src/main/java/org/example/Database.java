77603d5152c9097bafb5e090b6c2bfc20f78e02c
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-06-28 16:14:27 +0330

    updating database for rendering city and characters

--- src/main/java/org/example/Database.java
+++ src/main/java/org/example/Database.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import org.example.defualtSystem.Bank;
+import org.example.defualtSystem.Life;
 import org.example.defualtSystem.Municipality;
+import org.example.models.*;
 import org.example.models.Character;
-import org.example.models.Property;
-import org.example.models.User;
 
 import javax.xml.crypto.Data;
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class Database {
 
@@ -215,11 +221,210 @@ public class Database {
             statement.setString(5, jobTitle);
             statement.setString(6, inLocation);
 
-
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static ArrayList<Character> loadCharacter() {
+        ArrayList<Character> characters = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
+
+            // Execute a SELECT query
+            String sql = "SELECT * FROM characters";
+            ResultSet rs = statement.executeQuery(sql);
+
+            // Process the result set
+            while (rs.next()) {
+
+                String username = rs.getString("username");
+                String pass = rs.getString("password");
+                int money = rs.getInt("money");
+                String life = rs.getString("life");
+                String job = rs.getString("job");
+                String location = rs.getString("location");
+
+//                property arraylist ...
+                ArrayList<Property> ownPro = LoadProperties().stream()
+                        .filter(pro -> pro.getOwner().getUserInfo().getUsername().equals(username))
+                        .collect(Collectors.toCollection(ArrayList::new));
+
+//                life details
+                String[] Sep_life = life.split(",");
+                float food = Float.parseFloat(Sep_life[0]);
+                float sleep = Float.parseFloat(Sep_life[1]);
+                float water = Float.parseFloat(Sep_life[2]);
+
+                Life tempLife = new Life(food, water, sleep);
+//
+//                find char job
+                Job UserJob = null;
+                for (Job tempJob : loadJobs()) {
+                    if (tempJob.getTitle().equals(job)) {
+                        UserJob = tempJob;
+                    }
+                }
+
+//                find user bank account
+                BankAccount tempBankAccount = null;
+                for (BankAccount bankAccount : loadBankAccounts()) {
+                    if (bankAccount.getOwner().equals(username)) {
+                        tempBankAccount = bankAccount;
+                    }
+                }
+
+//                find in time coordinate
+                String[] Sep_cord = location.split(",");
+                float x = Float.parseFloat(Sep_cord[0]);
+                float y = Float.parseFloat(Sep_cord[1]);
+                Property tempProperty = null;
+                for (Property property : LoadProperties()) {
+                    if (property.getCoordinate()[1] == y && property.getCoordinate()[0] == x) {
+                        tempProperty = property;
+                    }
+                }
+
+//                create temp user
+                User tempUser = new User(username, pass);
+
+                Character character = new Character(tempUser, tempBankAccount, tempLife,UserJob ,ownPro,tempProperty );
+                characters.add(character);
+            }
+            rs.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return characters;
+    }
+
+    public static void createBankAccount(BankAccount bankAccount) {
+        String insertQuery = "INSERT INTO `bank-account`(`username`, `password`, `money`, `last`) VALUES (?,?,?,?)";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement statement = connection.prepareStatement(insertQuery)) {
+
+            String username = bankAccount.getOwner();
+            String password = bankAccount.getPassword();
+            float money = bankAccount.getMoney();
+            String last = bankAccount.getLastChange().toString();
+            statement.setString(1, username);
+            statement.setString(2, password);
+            statement.setFloat(3, money);
+            statement.setString(4, last);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<BankAccount> loadBankAccounts() {
+        String selectQuery = "SELECT * FROM `bank-account`";
+        ArrayList<BankAccount> bankAccounts = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement statement = connection.prepareStatement(selectQuery);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                float money = resultSet.getFloat("money");
+                String last = resultSet.getString("last");
+
+                String dateString = last;
+                DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy");
+                Date date = new Date();
+                try {
+                    LocalDateTime localDateTime = LocalDateTime.parse(dateString, dateFormat);
+                    date = Date.from(localDateTime.atZone(java.time.ZoneId.systemDefault()).toInstant());
+                } catch (DateTimeParseException e) {
+                    e.printStackTrace();
+                }
+
+                BankAccount bankAccount = new BankAccount(username, password, money, date);
+                bankAccounts.add(bankAccount);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return bankAccounts;
+    }
+
+    public static void saveJob(Job job) {
+        String insertQuery = "INSERT INTO `job`(`title`, `income`, `inid`) VALUES (?,?,?)";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement statement = connection.prepareStatement(insertQuery)) {
+
+            String title = job.getTitle();
+            float money = job.getIncome();
+            String IndustyID = job.getIndustryId();
+            statement.setString(1, title);
+            statement.setFloat(2, money);
+            statement.setString(3, IndustyID);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Job> loadJobs() {
+        String selectQuery = "SELECT * FROM `job`";
+        ArrayList<Job> jobs = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement statement = connection.prepareStatement(selectQuery);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                String title = resultSet.getString("title");
+                float income = resultSet.getFloat("income");
+                String industryId = resultSet.getString("inid");
+                Job job = new Job(title, income, industryId);
+                jobs.add(job);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return jobs;
+    }
+
+    public static void createCity(){
+        String insertQuery = "INSERT INTO `city`(`isCity`) VALUES (?)";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement statement = connection.prepareStatement(insertQuery)) {
+
+            statement.setBoolean(1, true);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean hasCity() {
+        String selectQuery = "SELECT COUNT(*) FROM `city` WHERE `isCity` = true";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement statement = connection.prepareStatement(selectQuery);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            if (resultSet.next()) {
+                int count = resultSet.getInt(1);
+                return count > 0;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
 }
